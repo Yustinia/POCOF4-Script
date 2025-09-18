1e339ed8cfad46bc22469df1a7262642fcdb342f
Yustinia <yustiniayevsa+development@gmail.com>
2025-09-18 21:01:42 +0800

    downloader & fd validation

---
+++ downloader.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dna_dir="/data/DNA"
+
+if [[ -f "$dna_dir/hosfix.zip" ]]; then
+    aria2c -j 8 -x 8 -s 8 --summary-interval=1 --console-log-level=warn --download-result=hide --continue=true --auto-file-renaming=true -d "$dna_dir" -o "hosfix.zip" "https://github.com/Yustinia/POCOF4-Script/releases/download/1.0.0/hyperos3-fixes.zip"
+fi
+
+7z x "$dna_dir/hosfix.zip" -o"hosfix/"
--- main.py
+++ main.py
@@ -3,3 +3,31 @@ from pathlib import Path
 
 # set global paths
 dna_dir = Path("/data/DNA")
+hosfix = Path("/data/DNA/hosfix")
+
+
+# checks whether the unpacked folders exists, else None
+def validate_fd_exists(folder_name):
+    fd_path = dna_dir / folder_name
+
+    if Path(fd_path).exists():
+        return fd_path
+    return None
+
+
+# existence
+def check_exists():
+    ent_f4_fd = input("Enter the F4 folder name: ").strip()
+    ent_port_fd = input("Enter the PORT folder name: ").strip()
+
+    missing_dirs = []
+
+    if not validate_fd_exists(ent_f4_fd):
+        missing_dirs.append("F4")
+    if not validate_fd_exists(ent_port_fd):
+        missing_dirs.append("PORT")
+
+    if missing_dirs:
+        print("Missing:", ", ".join(missing_dirs))
+        return False
+    return True
